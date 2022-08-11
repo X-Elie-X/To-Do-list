4ffdc5276d01a8b4722d56f2408df516c565969c
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-08-11 12:59:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import './style.css';
+import render from './populateui.js';
+import AddToDoList from './list.js';
 
-const todosList = [
-  { description: 'hello', completed: false, index: 1 },
-  { description: 'done', completed: false, index: 2 },
-];
+const taskList = new AddToDoList();
+render(taskList);
 
-const render = (list) => {
-  const sortedTodos = list.sort((a, b) => a.index - b.index);
-  const todosContainer = document.querySelector('.lists');
-  let todosHtml = '';
-  sortedTodos.forEach((todo) => {
-    todosHtml += ` <div class="todo-item">
-        <input type="checkbox" /><span> ${todo.description}</span>
-    </div>`;
-  });
-  todosContainer.innerHTML = todosHtml;
-};
+const addTodoBtn = document.querySelector('.add-btn');
+addTodoBtn.addEventListener('click', () => {
+  const id = `id${Math.random().toString(16).slice(2)}`;
+  const description = document.querySelector('.input-list').value.trim();
+  const completed = false;
+  const index = taskList.list.length + 1;
 
-render(todosList);
+  const newTask = {
+    id,
+    description,
+    completed,
+    index,
+  };
+  if (description) {
+    taskList.addTask(newTask);
+    render(taskList);
+  }
+});
