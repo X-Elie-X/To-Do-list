d848c45d614ca4d9d7600b17ec1dd7fd7e33011d
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-08-11 14:35:11 +0200

    Update list.js

--- src/list.js
+++ src/list.js
@@ -17,28 +17,4 @@ export default class AddToDoList {
     });
     localStorage.setItem('taskList', JSON.stringify(this.list));
   }
-
-  editTask(todoId, todoDescription) {
-    this.list = this.list.map((todo) => {
-      if (todo.id === todoId) {
-        return { ...todo, description: todoDescription };
-      }
-      return todo;
-    });
-    localStorage.setItem('taskList', JSON.stringify(this.list));
-  }
-
-  completeTask(todoId, status) {
-    const selected = this.list.findIndex((element) => element.id === todoId);
-    this.list[selected].completed = status;
-    localStorage.setItem('taskList', JSON.stringify(this.list));
-  }
-
-  clearCompletedTask() {
-    this.list = this.list.filter((todo) => !todo.completed);
-    this.list.forEach((todo, index) => {
-      todo.index = index + 1;
-    });
-    localStorage.setItem('taskList', JSON.stringify(this.list));
-  }
 }
